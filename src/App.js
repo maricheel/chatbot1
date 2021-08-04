14093703ea6d9373c7442bc9320ad491898e3876
Hicham elbanaji <Hicham1elbanaji@gmail.com>
2021-08-04 16:40:54 +0200

    sfvsfv

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import React from 'react';
+import Chatbot from 'react-chatbot-kit'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Chatbot/>
+    </div>
+  );
+}
+
+export default App;
